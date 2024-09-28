0a07a7ef6aaef414741de676d24005ade7069320
cassianosf <cassianosfranco@gmail.com>
2024-09-28 14:44:18 +0000

    Turborepo + Dev Container + Github setup

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+# sudo chmod +x ./.devcontainer/setup.sh
+# ./.devcontainer/setup.sh
+
+git config --global user.email "cassianosfranco@gmail.com"
+git config --global user.name "cassianosf"
+
+ssh-keygen -t rsa -b 4096 -C "cassianosfranco@gmail.com" -N "" -f /home/vscode/.ssh/id_rsa
+echo "Copy the following SSH key and add it to your Github Account:"
+cat /home/vscode/.ssh/id_rsa.pub
+echo "Press Enter to install dependencies..."
+read
+
+bun install
